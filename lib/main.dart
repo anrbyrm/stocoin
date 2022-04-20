6909714cd9980bfb3f0c20f53c2beea224d7564c
anrbyrm <anrbyrm@gmail.com>
2022-04-20 17:29:48 +0400

    instance initialize in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'package:stocoin/core/bootstrap/app.dart';
+import 'package:stocoin/core/config/app_localization.dart';
+import 'package:stocoin/core/localization/app_localization.dart';
 
-void main() {
+Future<void> main() async {
+  Localization.instance.init(
+    languageCode: languageCode,
+    valuesAsMap: valuesAsMap,
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     const App(
       mode: ThemeMode.system,
