8bf2ae5471af2159454a661302d2ddd25376533a
Anar <Anar@CALCULATOR>
2022-05-09 03:54:45 +0400

    constants file created

---
+++ lib/core/config/app_constants.dart
@@ -0,0 +1,11 @@
+/// double constants
+const double entrySymmetricVerticalPadding = 70;
+const double entrySymmetricHorizontalPadding = 24;
+const double entryButtonRadius = 16;
+
+/// int constants
+const int globalDuration = 900;
+
+/// route path
+const String loginPath = '/login';
+const String registerPath = '/register';
