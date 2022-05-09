1478e1d07097fab943f3edabe75b34ebf2d2cc95
Anar <Anar@CALCULATOR>
2022-05-09 04:00:30 +0400

    loading .env, injecting dependencies in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:stocoin/core/bootstrap/app.dart';
 import 'package:stocoin/core/config/app_localization.dart';
 import 'package:stocoin/core/localization/app_localization.dart';
+import 'package:stocoin/injectable.dart';
 
 Future<void> main() async {
-  Localization.instance.init(
+  await dotenv.load();
+  await Localization.instance.init(
     languageCode: languageCode,
     valuesAsMap: valuesAsMap,
   );
 
+  setupLocator();
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
