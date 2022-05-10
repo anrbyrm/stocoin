793ff4cc11f5ab24e3134be6bf9b0cd8354a7f91
anrbyrm <anrbyrm@gmail.com>
2022-05-10 20:46:12 +0400

    inject dependencies

--- lib/injectable.dart
+++ lib/injectable.dart
@@ -1,9 +1,12 @@
 import 'package:get_it/get_it.dart';
 
 import 'package:stocoin/core/bootstrap/navigation_service.dart';
+import 'package:stocoin/features/login/presentation/cubit/login_cubit.dart';
 
 final di = GetIt.instance;
 
 void setupLocator() {
-  di.registerFactory<NavigationService>(() => NavigationService.instance);
+  di
+    ..registerFactory<NavigationService>(() => NavigationService.instance)
+    ..registerFactory<LoginCubit>(() => LoginCubit());
 }
