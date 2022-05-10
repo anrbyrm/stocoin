96df3b1fca52e5293f2d5f3da6345065c9cf3332
anrbyrm <anrbyrm@gmail.com>
2022-05-10 20:47:49 +0400

    login cubit created

--- lib/features/login/presentation/cubit/login_cubit.dart
+++ lib/features/login/presentation/cubit/login_cubit.dart
@@ -1,8 +1,33 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+
+import 'package:stocoin/core/models/entry_models.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit(LoginState initialState) : super(initialState);
+  LoginCubit() : super(LoginState.initial());
+
+  void emailChanged(String? value) {
+    final email = Email.dirty(value);
+
+    emit(
+      state.copyWith(
+        email: email,
+        status: Formz.validate([email, state.password!]),
+      ),
+    );
+  }
+
+  void passwordChanged(String? value) {
+    final password = Password.dirty(value);
+
+    emit(
+      state.copyWith(
+        password: password,
+        status: Formz.validate([state.email!, password]),
+      ),
+    );
+  }
 }
--- lib/features/login/presentation/cubit/login_state.dart
+++ lib/features/login/presentation/cubit/login_state.dart
@@ -1,7 +1,33 @@
 part of 'login_cubit.dart';
 
 class LoginState extends Equatable {
+  const LoginState({
+    this.email,
+    this.password,
+    this.status,
+  });
+
+  final Email? email;
+  final Password? password;
+  final FormzStatus? status;
+
+  factory LoginState.initial() => LoginState(
+        email: Email.pure(),
+        password: Password.pure(),
+        status: FormzStatus.pure,
+      );
+
+  LoginState copyWith({
+    Email? email,
+    Password? password,
+    FormzStatus? status,
+  }) =>
+      LoginState(
+        email: email ?? this.email,
+        password: password ?? this.password,
+        status: status ?? this.status,
+      );
+
   @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [email, password, status];
 }
