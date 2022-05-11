7dc9809ac315b4b29ca1c1c30fb6f66ebecd9d7b
anrbyrm <anrbyrm@gmail.com>
2022-05-11 18:47:37 +0400

    logo commented out

--- lib/features/login/presentation/widgets/logo.dart
+++ lib/features/login/presentation/widgets/logo.dart
@@ -1,23 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_hooks/flutter_hooks.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
-import 'package:stocoin/core/bootstrap/helpers.dart';
+// import 'package:stocoin/core/bootstrap/helpers.dart';
+// // import 'package:stocoin/core/ui/particle/particle_factory.dart';
 
-class Logo extends HookWidget {
-  const Logo({Key? key}) : super(key: key);
+// class Logo extends HookWidget {
+//   const Logo({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height / 5,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          SvgPicture.asset(getIcon('logo.svg')),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//   }
+// }
